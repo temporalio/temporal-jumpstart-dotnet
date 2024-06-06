5d4f8a672bfc482434e4e942e44f38ee2f13bf95
Mike Nichols <mike.nichols@temporal.io>
2024-06-06 08:45:02 -0400

    working out testing

--- tests/Temporal.Curriculum.Workflows/Temporal.Curriculum.Workflows.Tests/Orchestrations/StartOnboardingTests.cs
+++ tests/Temporal.Curriculum.Workflows/Temporal.Curriculum.Workflows.Tests/Orchestrations/StartOnboardingTests.cs
@@ -1,12 +1,33 @@
+using Temporal.Curriculum.Workflows.Domain.Orchestrations;
+using Temporal.Curriculum.Workflows.Messages.Orchestrations;
+using Temporalio.Client;
+using Temporalio.Worker;
+
 namespace Temporal.Curriculum.Workflows.Tests.Orchestrations;
 using Xunit;
 using Xunit.Abstractions;
+using Temporalio.Testing;
 public class StartOnboardingTests : TestBase
 {
-    [TimeSkippingServerFact]
+    [Fact]
     public async Task RunAsync_SimpleRun_Succeeds()
     {
         await using var env = await WorkflowEnvironment.StartTimeSkippingAsync();
+        var wid = Guid.NewGuid();
+        var args = new OnboardEntityRequest(wid.ToString(), "test");
+        using var worker = new TemporalWorker(
+            env.Client,
+            new TemporalWorkerOptions("test").AddWorkflow<OnboardEntity>());
+
+        await worker.ExecuteAsync(async () =>
+        {
+            await env.Client.ExecuteWorkflowAsync(
+                (OnboardEntity wf) => wf.ExecuteAsync(args),
+                new WorkflowOptions(id: args.Id, taskQueue: worker.Options.TaskQueue!));
+            Assert.Equal(42, 42);
+        });
+        
+        
         // var myActivities = new MyActivities();
         // using var worker = new TemporalWorker(
         //     env.Client,
@@ -22,5 +43,9 @@ public class StartOnboardingTests : TestBase
         //         new(id: $"workflow-{Guid.NewGuid()}", taskQueue: worker.Options.TaskQueue!));
         //     Assert.Equal("some-static-value", result);
         // });
+    }
+
+    public StartOnboardingTests(ITestOutputHelper output) : base(output)
+    {
     }
 }
--- tests/Temporal.Curriculum.Workflows/Temporal.Curriculum.Workflows.Tests/TestBase.cs
+++ tests/Temporal.Curriculum.Workflows/Temporal.Curriculum.Workflows.Tests/TestBase.cs
@@ -1,49 +1,39 @@
-// namespace Temporal.Curriculum.Workflows.Tests;
-//
-// using Microsoft.Extensions.Logging;
-// using Xunit.Abstractions;
-//
-// public abstract class TestBase : IDisposable
-// {
-//     private readonly TextWriter? consoleWriter;
-//
-//     protected TestBase(ITestOutputHelper output)
-//     {
-//         if (Program.InProc)
-//         {
-//             LoggerFactory = Microsoft.Extensions.Logging.LoggerFactory.Create(builder =>
-//                 builder.
-//                     AddSimpleConsole(options => options.TimestampFormat = "[HH:mm:ss] ").
-//                     SetMinimumLevel(Program.Verbose ? LogLevel.Trace : LogLevel.Information));
-//         }
-//         else
-//         {
-//             LoggerFactory = Microsoft.Extensions.Logging.LoggerFactory.Create(builder =>
-//                 builder.AddXUnit(output));
-//             // Only set this if not in-proc
-//             consoleWriter = new ConsoleWriter(output);
-//             Console.SetOut(consoleWriter);
-//         }
-//     }
-//
-//     ~TestBase()
-//     {
-//         Dispose(false);
-//     }
-//
-//     protected ILoggerFactory LoggerFactory { get; private init; }
-//
-//     public void Dispose()
-//     {
-//         Dispose(true);
-//         GC.SuppressFinalize(this);
-//     }
-//
-//     protected virtual void Dispose(bool disposing)
-//     {
-//         if (disposing)
-//         {
-//             consoleWriter?.Dispose();
-//         }
-//     }
-// }
+namespace Temporal.Curriculum.Workflows.Tests;
+
+using Microsoft.Extensions.Logging;
+using Xunit.Abstractions;
+
+public abstract class TestBase : IDisposable
+{
+    private readonly TextWriter? _consoleWriter;
+
+    protected TestBase(ITestOutputHelper output)
+    {
+        LoggerFactory = Microsoft.Extensions.Logging.LoggerFactory.Create(builder =>
+            builder.AddXUnit(output));
+        // Only set this if not in-proc
+        _consoleWriter = new ConsoleWriter(output);
+        Console.SetOut(_consoleWriter);
+    }
+
+    ~TestBase()
+    {
+        Dispose(false);
+    }
+
+    protected ILoggerFactory LoggerFactory { get; private init; }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            _consoleWriter?.Dispose();
+        }
+    }
+}
